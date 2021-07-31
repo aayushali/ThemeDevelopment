3261f72351aefd7d398f8aa911fac1e77c1886a4
aayushali <aayushkhan51@gmail.com>
2021-07-31 20:55:50 +0545

    Started plugin development

---
+++ wp-content/plugins/aayush-practice/aayush-practise.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package AayushPlugin
+ * Plugin Name: Aayush Practice Plugin
+ * Plugin URI: http://aayushali.com.np/plugin
+ * Description: This is my first attempt on writing a custom plugin.
+ * Version: 1.0.0
+ * Author: Aayush Ali
+ * Author URI: https://aayushali.com.np
+ * License: GPLv2 or later
+ * Text Domain: aayush-plugin
+ */
